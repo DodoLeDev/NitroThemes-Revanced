8ee7c8497fa4f11e9f558cf667c9ccba2053fcbd
DodoLeDev <59477313+DodoLeDev@users.noreply.github.com>
2023-06-06 00:09:05 +0200

    Maybe final experiments?

--- plugins/ChromaGlow/src/index.ts
+++ plugins/ChromaGlow/src/index.ts
@@ -1,12 +1,8 @@
 import { findByProps } from "@vendetta/metro"
-import { instead } from "@vendetta/patcher"
 
-const canUse = findByProps("canUseClientThemes")
 const { default: { setShouldSyncAppearanceSettings }, saveClientTheme } = findByProps("saveClientTheme")
 
 const patches = [
-    instead("canUseClientThemes", canUse, () => true),
-    instead("canUsePremiumProfileCustomization", canUse, () => true),
     setTimeout(() => {
         saveClientTheme({
             backgroundGradientPresetId: 9,
