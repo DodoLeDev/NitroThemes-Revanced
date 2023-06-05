74759bb91aebe9ed881ccd4253403bde6abf170f
DodoLeDev <59477313+DodoLeDev@users.noreply.github.com>
2023-06-05 23:42:31 +0200

    Fix

--- plugins/ThemeDebugTools/src/index.ts
+++ plugins/ThemeDebugTools/src/index.ts
@@ -1,6 +1,33 @@
 import { findByProps } from "@vendetta/metro"
-import { ApplicationCommandType, ApplicationCommandInputType, ApplicationCommandOptionType } from "../../../ApplicationCommandTypes"
 import { registerCommand } from "@vendetta/commands"
+
+export const enum ApplicationCommandInputType {
+    BUILT_IN,
+    BUILT_IN_TEXT,
+    BUILT_IN_INTEGRATION,
+    BOT,
+    PLACEHOLDER
+}
+
+export const enum ApplicationCommandOptionType {
+    SUB_COMMAND = 1,
+    SUB_COMMAND_GROUP,
+    STRING,
+    INTEGER,
+    BOOLEAN,
+    USER,
+    CHANNEL,
+    ROLE,
+    MENTIONABLE,
+    NUMBER,
+    ATTACHMENT
+}
+
+export const enum ApplicationCommandType {
+    CHAT = 1,
+    USER,
+    MESSAGE
+}
 
 const { BackgroundGradientPresetId } = findByProps("BackgroundGradientPresetId")
 const ClydeUtils = findByProps("sendBotMessage")
